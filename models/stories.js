0d94edc01d0fe79dd99fe45ffcad426a06fa9386
Robin <robinlooup@gmail.com>
2025-02-04 11:26:40 +0100

    modif schema

--- models/stories.js
+++ models/stories.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const storySchema = mongoose.Schema({
-    writer: { type: mongoose.Schema.Types.ObjectId, ref: 'users' }, // foreign key : user qui a créé la story
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'users' }, // foreign key : user qui a créé la story
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }], // foreign key : tableau des users qui ont liké l'event
     createdAt: Date,
     title: String, // Autre écriture rencontrée à confirmer : {type:String, require: true},
